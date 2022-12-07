f74c46adc269b47fdc332ff68726000a5e202a41
Isabella Scalzi <isabellascalzi@gmail.com>
2022-12-07 19:50:29 +1030

    Day 5

--- 05/02/Program.cs
+++ 05/02/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+// TODO: Parse fine input first and split it into two lines, starting and actions
 List<string> starting = new List<string>();
 
 using(StreamReader sr = new StreamReader(@"input.txt")) {
@@ -8,30 +9,32 @@ using(StreamReader sr = new StreamReader(@"input.txt")) {
       starting.Add(line);
       line = sr.ReadLine();
    } 
-
-   int startingNoLines = starting.Count;
+   
+   // Create the starting stacks
    int noStacks = (starting[^1].Length / 4) + 1;
-
    List<Stack<char>> stacks = new List<Stack<char>>();
    for(int i=0; i<noStacks; i++) {
       stacks.Add(new Stack<char>());
    }
 
-   List<string> blah = starting.GetRange(0, starting.Count - 1);
-   blah.Reverse();
-   // blah.ForEach(b => Console.WriteLine(b.Length));
+   List<string> stacksInput = starting.GetRange(0, starting.Count - 1);
+   stacksInput.Reverse();
 
    // Get each character into its respective stack
-   foreach(string l in blah) {
+   foreach(string l in stacksInput) {
       for(int i=1, j=0; i<l.Length; i+=4, j++) {
          if(!char.IsWhiteSpace(l[i])) {
-            Console.WriteLine($"Push {l[i]} onto stack no {j}");
             stacks[j].Push(l[i]);
          }
       }
    }
 
    line = sr.ReadLine();
+   
+   // Keep a list of crates that are popped off in each step so we can then push them onto the new stack in order
+   List<char> crates = new List<char>();
+
+   // Parse and perform each crane movement
    while(line != null) {
       string[] lineTokens = line.Split(' ');
       int noCratesToMove = int.Parse(lineTokens[1]);
@@ -40,23 +43,21 @@ using(StreamReader sr = new StreamReader(@"input.txt")) {
       
       for(int i=0; i<noCratesToMove; i++) {
          char temp = stacks[stackToMoveFrom].Pop(); 
-         stacks[stackToMoveTo].Push(temp);
+         crates.Add(temp);
       }
-      Console.WriteLine($"Move {noCratesToMove} from stack {stackToMoveFrom} to stack {stackToMoveTo}");
+      
+      for(int i=crates.Count-1; i>=0; i--) {
+         stacks[stackToMoveTo].Push(crates[i]);
+      }
 
+      crates.Clear();
       line = sr.ReadLine();
    }
+   
+   // Print output
    string res = "";
    for(int i=0; i<noStacks; i++) {
       res += stacks[i].Peek();
    }
    Console.WriteLine(res);
 }
-
-
-// foreach(Stack<char> stack in stacks) {
-//    Console.WriteLine("New Stack");
-//    foreach(char c in stack) {
-//       Console.WriteLine(c);
-//    }
-// }
